c283e0b048a414d22e63f374d9d9cdcba1d1afd0
João <joaogui.silvae@gmail.com>
2026-01-19 13:08:16 -0300

    Termino do projeto

--- countdown/src/App.jsx
+++ countdown/src/App.jsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Outlet } from "react-router-dom";
+import { useContext } from "react";
+import { CountdownContext } from "./context/CountdownContext";
+
+import backgroundImg from "./img/background.jpg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { event } = useContext(CountdownContext);
+
+  let eventImage = null;
+
+  if (event) eventImage = event.image;
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div
+      className="App"
+      style={
+        eventImage
+          ? { backgroundImage: `url(${eventImage})` }
+          : { backgroundImage: `url(${backgroundImg})` }
+      }
+    >
+      <div className="container">
+        <Outlet />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
